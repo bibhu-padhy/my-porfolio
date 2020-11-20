0a5e4a0285a0975af22ce6d6fc9bc0c13d880857
bibhu-padhy <bibhupadhy140@gmail.com>
2020-11-20 08:12:11 +0530

    build issue fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
