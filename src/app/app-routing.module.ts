ad6a42c5dbbe1a5c35b4d62058bcc043c75aab97
bibhu-padhy <bibhupadhy140@gmail.com>
2020-11-21 16:58:40 +0530

    home screen content and design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('src/app/view/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
